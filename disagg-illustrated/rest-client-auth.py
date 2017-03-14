7db96e6e6f46930a9b07ec68fa166b52e701ea6a
Tom Ammon <tammon@ldschurch.org>
2017-03-13 22:19:12 -0600

    updated mycustomapi version 1 with new auth code

---
+++ disagg-illustrated/rest-client-auth.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python
+import requests
+import json
+
+url = "http://203.0.113.3/api/v0/mycustomapi.php/testrequest"
+header = {"content-type":"application/json", "X-DisaggBlog-API-Key":"MyBrokenSecretKey"}
+
+apiresponse = requests.get(url, headers=header, verify=False)
+
+print (json.dumps(apiresponse.json(), indent=4, separators=(',',': ')))
--- disagg-illustrated/v1/mycustomapi.php
+++ disagg-illustrated/v1/mycustomapi.php
@@ -1,5 +1,8 @@
 <?php
 
+// specify the API key
+$APIKEY = "MySecretKey";
+
 // find out which http verb was used in the request
 $method = $_SERVER['REQUEST_METHOD'];
 
@@ -15,22 +18,32 @@ $headers = apache_request_headers();
 $methoderr = array("MethodError" => "Method not supported");
 $notfound = array("NotFound" => "API Call not supported");
 
-if ($method == "GET"){ // if the method was GET, its okay to send data back to the requestor
-	switch ($request[0]){
-		case "testrequest": //if the first part of the URL was "testrequest"
-			//then create an array with some static text in it and send it back to the
-			//requestor as JSON
-			$statictext = array("Testing" => "Your call to testrequest was successful");
-			echo json_encode($statictext);
-			break;
+//add an authentication failed message
+$autherr = array("AuthenticationError" => "Authentication failed");
 
-		default: //if the first part of the URL was not found in this switch statement
-			//then send back our canned error message
-			echo json_encode($notfound);
-	}
+if (in_array($APIKEY, $headers)){ //if the API key is in the http header, continue trying to service the API call
+	if ($method == "GET"){ // if the method was GET, its okay to send data back to the requestor
+		switch ($request[0]){
+			case "testrequest": //if the first part of the URL was "testrequest"
+				//then create an array with some static text in it and send it back to the
+				//requestor as JSON
+				$statictext = array("Testing" => "Your call to testrequest was successful");
+				echo json_encode($statictext);
+				break;
 
-} else { // if the method was anything other than GET, send back a JSON block containing our canned error message
-	echo json_encode($methoderr);
+			default: //if the first part of the URL was not found in this switch statement
+				//then send back our canned error message
+				echo json_encode($notfound);
+			}
+
+		} else { // if the method was anything other than GET, send back a JSON block containing our canned error message
+			echo json_encode($methoderr);
+		}
+
+} else { // if the API key was not found, return an error
+        echo json_encode($autherr);
 }
+
+
 
 ?>
